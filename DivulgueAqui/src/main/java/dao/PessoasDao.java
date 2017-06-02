be768774e3439b4e7fb012a79e393308482a58a7
Anderson Felipe <andersonfseabra3@gmail.com>
2017-06-02 00:42:10 -0300

    projeto voltando a funcionar ( aos poucos) o maven esta funcionando, web service inserindo usuario ( ate agora). retirado diretorios do .gitignore

---
+++ DivulgueAqui/src/main/java/dao/PessoasDao.java
@@ -0,0 +1,108 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import hibernate.HibernateUtil;
+import entidade.PessoaEntidade;
+import java.util.List;
+import javax.persistence.EntityManager;
+
+
+/**
+ *
+ * @author Izaquias
+ */
+public class PessoasDao implements DaoGenerico<PessoaEntidade>{
+
+    private static EntityManager manager;
+
+    public PessoasDao() {
+    }
+    
+    
+    @Override
+    public void inserir(PessoaEntidade p){
+           manager = HibernateUtil.getInstance().getFactory().createEntityManager();
+           PessoasDao.manager.getTransaction().begin();
+       
+       try{
+           PessoasDao.manager.persist(p);
+           PessoasDao.manager.getTransaction().commit();
+           System.out.println("Dados armazenados com sucesso!");
+       }catch(IllegalArgumentException iae){
+           PessoasDao.manager.getTransaction().rollback();
+           System.out.println("Ocorreu algo inexperado!");
+           //throw new IllegalArgumentException("Dados inválidos, tente novamente, reveja a operação!");
+       } finally{
+           
+           //Pessoas.manager.getTransaction().rollback();
+           PessoasDao.manager.close();
+           System.out.println("Fim da operação!");
+       }       
+    }   
+    
+
+    @Override
+    public void alterar(PessoaEntidade p) {
+        manager = HibernateUtil.getInstance().getFactory().createEntityManager();
+        PessoasDao.manager.getTransaction().begin();
+        
+        try {
+          PessoasDao.manager.merge(p);
+          PessoasDao.manager.getTransaction().commit();
+          System.out.println("Dados alterados com sucesso!");
+        } catch (Exception e) {
+          PessoasDao.manager.getTransaction().rollback();
+            System.out.println("Não foi possível realizar esta alteração!");
+        }finally{
+            PessoasDao.manager.close();
+            System.out.println("Fim da sessão!");
+            
+        }
+        
+    }
+
+    @Override
+    public void remover(PessoaEntidade p) {
+        manager = HibernateUtil.getInstance().getFactory().createEntityManager();
+        PessoasDao.manager.getTransaction().begin();
+        try {
+            p = PessoasDao.manager.find(PessoaEntidade.class, p.getId());
+            PessoasDao.manager.remove(p);
+            PessoasDao.manager.getTransaction().commit();
+            System.out.println("Registro removido com sucesso!");
+        } catch (Exception e) {
+            PessoasDao.manager.getTransaction().rollback();
+            System.out.println("Não foi possível remover este registro!");
+        }finally{
+            PessoasDao.manager.close();
+            System.out.println("Fim da sessão!");
+        }
+    }
+
+    @Override
+    public PessoaEntidade recuperar(Long id) {
+        manager = HibernateUtil.getInstance().getFactory().createEntityManager();
+        PessoasDao.manager.getTransaction().begin();
+        
+        try {
+           return (PessoaEntidade) PessoasDao.manager.find(PessoaEntidade.class, id);
+        } catch (Exception e) {
+            System.out.println("id não encontrado!");
+            System.out.println(e.getMessage());
+        }finally{
+            PessoasDao.manager.close();
+            System.out.println("Fim da sessão!");
+        }
+        return null;
+    }
+
+    @Override
+    public List<PessoaEntidade> recuperarTodos() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
