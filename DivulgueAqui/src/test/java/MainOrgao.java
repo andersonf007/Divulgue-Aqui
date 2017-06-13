69d65a588c373ab4177b5e07d3b0617ee7b74882
izaquias cavalcante <izaquiascavalcante@gmail.com>
2017-06-13 13:51:13 -0300

    Projeto base!

---
+++ DivulgueAqui/src/test/java/MainOrgao.java
@@ -0,0 +1,113 @@
+
+import com.google.gson.Gson;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import javax.swing.JOptionPane;
+import org.json.simple.JSONObject;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ander
+ */
+public class MainOrgao {
+    
+    public static void main(String[] args) {
+         ///////////////////////////ORGAO-INSERIR/////////////////////////////////// 
+      
+        String nome = "compesa";
+        String senha = "321";
+          
+        JSONObject jsonObject = new JSONObject();
+
+        //Armazena dados em um Objeto JSON
+        jsonObject.put("nome", nome);
+        jsonObject.put("senha", senha);
+           
+        Gson gson = new Gson();
+        String Json = gson.toJson(jsonObject);
+
+        URL url;
+        try {
+            url = new URL("http://localhost:8084/DivulgueAqui/webresources/webService/orgao/inserir");
+
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoOutput(true);
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/json");
+
+            try (OutputStream os = connection.getOutputStream()) {
+                os.write(Json.getBytes("UTF-8"));
+                os.flush();
+                
+                int code = connection.getResponseCode();
+                System.out.println(code + " - " + Json);
+            }
+            connection.disconnect();
+
+            } catch (MalformedURLException ex) {
+                JOptionPane.showMessageDialog(null, "erro de URLException conexao ao rest ( salvar cliente)\n" + ex);
+            } catch (IOException ex) {
+                JOptionPane.showMessageDialog(null, "erro de IOException conexao ao rest ( salvar cliente) \n" + ex);
+            }
+        
+     ///////////////////////////ORGAO-RECUPERAR/////////////////////////////////// 
+     /*
+        String nome ;
+        String senha;
+        long codigo = 2;
+            URL url;
+        try {
+            url = new URL("http://localhost:8084/web/webresources/webService/orgao/recuperar?id="+codigo);
+
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoInput(true);
+            connection.setDoOutput(true);
+            connection.setRequestMethod("GET");
+          
+            int code = connection.getResponseCode();
+            System.out.println(code);
+
+            InputStream inputStrem = connection.getInputStream();
+            BufferedReader br =  new BufferedReader(new InputStreamReader(inputStrem));
+            
+            String a;
+            StringBuilder stringBuilder = new StringBuilder();
+            while ((a  = br.readLine()) != null){
+             //a += br.readLine();
+             stringBuilder.append(a);
+            }
+          //  System.out.println(stringBuilder.toString());
+            connection.disconnect();
+            
+             JSONObject jsonObject;
+       
+             JSONParser parser = new JSONParser();  
+      
+            jsonObject = (JSONObject) parser.parse(stringBuilder.toString());
+            
+            codigo = (long) jsonObject.get("codigo");
+            nome = (String) jsonObject.get("nome");
+            senha = (String) jsonObject.get("senha");
+                        
+            System.out.println("o codigo é :" + codigo + " nome : " + nome 
+            + " senha : " + senha);
+
+        } catch (MalformedURLException ex) {
+            JOptionPane.showMessageDialog(null, "erro de URLException conexao ao rest ( recuperar usuario)\n" + ex);
+        } catch (IOException ex) {
+            JOptionPane.showMessageDialog(null, "erro de IOException conexao ao rest ( Recuperar usuario) \n" + ex);
+        } catch (ParseException ex) {
+            JOptionPane.showMessageDialog(null, "erro de ParseException conexao ao rest ( Recuperar usuario) \n" + ex);
+        }
+         */    
+    }
+}
